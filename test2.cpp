7cf7c72ede892bfe9921cd017ae086059bff22a9
KingPhilip14 <kingian404@gmail.com>
2024-12-16 15:05:34 -0600

    Trying new solution with new guest ready semaphore

--- test2.cpp
+++ test2.cpp
@@ -14,6 +14,7 @@ const int NUM_ROOMS = 3;
 sem_t check_in_available_sem;
 sem_t check_out_available_sem;
 sem_t available_room_sem;
+sem_t guest_ready_sem;
 sem_t mutex;
 
 queue<int> available_room_queue;
@@ -25,10 +26,13 @@ int total_guests = 0;
 void* guest(void* arg) {
     int guest_id = *(int*)arg;
 
+    // a guest can only enter the hotel if a room is available
     sem_wait(&available_room_sem);
 
+    // the guest enters the hotel
     printf("Guest %d enters the hotel.\n", guest_id);
 
+    // Guest waits to be served by the check-in receptionist
     sem_wait(&check_in_available_sem);
     sem_wait(&mutex);
 
@@ -40,15 +44,16 @@ void* guest(void* arg) {
     sem_post(&mutex);
     sem_post(&check_in_available_sem);
 
+    // Guest activity
     int rand_activity_index = rand() % guest_activities.size();
 
     sem_wait(&mutex);
     activity_counts[rand_activity_index]++;
     sem_post(&mutex);
-
     printf("Guest %d goes to the %s for their activity.\n", guest_id, guest_activities[rand_activity_index].c_str());
     sleep(rand() % 3 + 1);
 
+    // Guest check-out
     sem_wait(&check_out_available_sem);
     sem_wait(&mutex);
 
@@ -65,6 +70,9 @@ void* guest(void* arg) {
     sem_wait(&mutex);
     total_guests++;
     sem_post(&mutex);
+
+    // Signal the next guest to enter
+    sem_post(&guest_ready_sem);
 
     pthread_exit(nullptr);
 }
@@ -96,6 +104,7 @@ int main() {
     sem_init(&check_in_available_sem, 0, 1);
     sem_init(&check_out_available_sem, 0, 1);
     sem_init(&mutex, 0, 1);
+    sem_init(&guest_ready_sem, 0, 0); // Initialize guest_ready_sem to 0, so first guest can enter
 
     for (int i = 0; i < NUM_ROOMS; i++) {
         available_room_queue.push(i);
@@ -111,11 +120,19 @@ int main() {
     for (int i = 0; i < NUM_GUESTS; i++) {
         guest_ids[i] = i;
         pthread_create(&guests[i], nullptr, guest, &guest_ids[i]);
+
+        // After creating a guest thread, signal that they are ready to enter the hotel
+        sem_post(&guest_ready_sem);
     }
 
+    // Wait for all guests to finish
     for (int i = 0; i < NUM_GUESTS; i++) {
         pthread_join(guests[i], nullptr);
     }
+
+    // Terminate receptionist threads
+    pthread_cancel(check_in_thread);
+    pthread_cancel(check_out_thread);
 
     printf("\n--- Ending Information ---\n");
     printf("Total Guests: %d\n", total_guests);
@@ -124,10 +141,12 @@ int main() {
         printf("%s: %d\n", guest_activities[i].c_str(), activity_counts[i]);
     }
 
+    // Clean up
     sem_destroy(&available_room_sem);
     sem_destroy(&check_in_available_sem);
     sem_destroy(&check_out_available_sem);
     sem_destroy(&mutex);
+    sem_destroy(&guest_ready_sem);
 
     return 0;
 }
